117571c5c491b5df874743358ee748b0ceb29a8e
camylla-araujo <camylla.a_@hotmail.com>
2022-10-03 08:47:44 -0300

    Revisão de String

---
+++ index.js
@@ -0,0 +1,11 @@
+// Faça aparecer no console.log ''Sofia21" sem digitar ''21'' utilizando somente:
+//indexOf
+//split
+//join
+// +
+//substring
+
+//let te = "0123456789";
+let texto = "Sofia Manzano";
+
+console.log(texto.substring(0,5) + texto.indexOf("f") + texto.indexOf("o"));
